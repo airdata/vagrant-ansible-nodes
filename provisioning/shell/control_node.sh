038cc1399bab30ea2013328d806537f403a6eaab
Rumen Lishkov <airdata@abv.bg>
2022-12-23 03:03:44 +0200

    Add port fw

--- provisioning/shell/control_node.sh
+++ provisioning/shell/control_node.sh
@@ -2,9 +2,6 @@
 
 # this script installs ansible control node 
 
-ANSIBLE_PACKAGE="ansible"
-GIT_PACKAGE="git"
-EPEL_REPO_PACKAGE="epel-release"
 PLATFORM_RELEASE_FILE="/etc/redhat-release"
 ANSIBLE_HOSTS_FILE="/etc/ansible/hosts"
 ANSIBLE_NODES=("$@")
@@ -13,38 +10,6 @@ function platform_supported() {
     [ -f "$PLATFORM_RELEASE_FILE" ] && \
     echo "Platform supported" || \
     (echo "Platform is not supported. Exiting..."; exit 1)
-}
-
-function enable_epel_repo() {
-    check_installed "$EPEL_REPO_PACKAGE"
-    if [ "$?" -eq 1 ]; then
-        echo "$EPEL_REPO_PACKAGE is not enabled"
-        install_package "$EPEL_REPO_PACKAGE"
-    fi
-}
-
-function install_ansible() {
-    check_installed "$ANSIBLE_PACKAGE"
-    if [ "$?" -eq 1 ]; then
-        echo "$ANSIBLE_PACKAGE is not installed"
-        install_package "$ANSIBLE_PACKAGE"
-    fi
-}
-
-function install_git() {
-    check_installed "$GIT_PACKAGE"
-    if [ "$?" -eq 1 ]; then
-        echo "$GIT_PACKAGE is not installed"
-        install_package "$GIT_PACKAGE"
-    fi
-}
-
-function check_installed() {
-    yum list installed "$@"
-}
-
-function install_package() {
-    yum install "$@" -y
 }
 
 function chmod_ssh() {
@@ -62,11 +27,11 @@ function install_docker () {
     # Remove any old versions
     sudo yum remove docker docker-common docker-selinux docker-engine
     # Install required packages
-    sudo yum install -y yum-utils device-mapper-persistent-data lvm2
+    sudo yum install -y yum-utils epel-release device-mapper-persistent-data lvm2
     # Configure docker repository
     sudo yum-config-manager --add-repo https://download.docker.com/linux/centos/docker-ce.repo
     # Install Docker-ce
-    sudo yum install -y docker-ce
+    sudo yum install -y docker-ce ansible git
     # Start Docker
     sudo systemctl start docker
     sudo systemctl enable docker
@@ -102,11 +67,8 @@ function install_jenkins () {
 }
 
 if platform_supported; then
-    enable_epel_repo && \
-    install_ansible && \
-    install_git && \
-    add_ansible_nodes && \
     chmod_ssh && \
     install_docker && \
+    add_ansible_nodes && \
     install_jenkins
 fi
